5be108c27785643506db9ce6cce0a82568a11968
eridani9k <eridani9k@gmail.com>
2023-01-18 08:20:42 +0800

    added tests for step()

--- ds/round_robin.go
+++ ds/round_robin.go
@@ -1,7 +1,7 @@
 package ds
 
 import (
-	"fmt"
+//"fmt"
 )
 
 type RoundRobin struct {
@@ -17,6 +17,7 @@ func NewRoundRobin(addrs []string) *RoundRobin {
 	}
 
 	var curr int
+	var next int
 	if len(addrs) == 1 {
 		next = curr
 	} else {
@@ -36,6 +37,20 @@ func (r *RoundRobin) Add(addr string) {
 	r.size += 1
 }
 
-func (r *RoundRobin) Size() {
+func (r *RoundRobin) Size() int {
 	return r.size
+}
+
+func (r *RoundRobin) Step(index int) int {
+	return step(r.addrs, index)
+}
+
+// TODO: abstract this further to step(max int, index int).
+// Passing the slice is unnecessary.
+func step(s []string, index int) int {
+	if index == len(s)-1 {
+		return 0
+	}
+
+	return index + 1
 }
--- ds/round_robin_test.go
+++ ds/round_robin_test.go
@@ -3,3 +3,41 @@ package ds
 import (
 	"testing"
 )
+
+func TestStep(t *testing.T) {
+	tests := map[string]struct {
+		s     []string
+		index int
+		want  int
+	}{
+		"increment_nonlooping_01": {
+			s:     []string{"a", "b", "c", "d"},
+			index: 0,
+			want:  1,
+		},
+		"increment_nonlooping_02": {
+			s:     []string{"a", "b", "c", "d"},
+			index: 1,
+			want:  2,
+		},
+		"increment_looping": {
+			s:     []string{"a", "b", "c", "d"},
+			index: 3,
+			want:  0,
+		},
+		"single_element": {
+			s:     []string{"a"},
+			index: 0,
+			want:  0,
+		},
+	}
+
+	for name, ts := range tests {
+		t.Run(name, func(t *testing.T) {
+			got := step(ts.s, ts.index)
+			if got != ts.want {
+				t.Errorf("got: %+v, want: %+v", got, ts.want)
+			}
+		})
+	}
+}
