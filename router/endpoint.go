e757dcc7558094cabd61ca5417d269c75cd61b22
eridani9k <eridani9k@gmail.com>
2023-01-24 10:47:55 +0800

    improved comments

--- router/endpoint.go
+++ router/endpoint.go
@@ -3,7 +3,16 @@ package router
 // endpoint is an address definition of a server
 // fronted by the Router.
 type endpoint struct {
-	addr    string
+
+	// addr is a fully qualified address of a backend
+	// server. This value mirrors that found in the
+	// addresses.cfg file.
+	addr string
+
+	// healthy indicates if this endpoint is ready to
+	// receive requests. This value is modified when ever
+	// a change in connectivity is detected, either through
+	// a health check, or a failed proxy request.
 	healthy bool
 }
 
